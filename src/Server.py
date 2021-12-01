6d603a89fda0d55ea89ce4cdbb71edef111e528a
RaduSoare <radu.soare23@yahoo.com>
2021-12-01 15:33:21 +0200

    Refactor after City and Country implemantation

--- src/Server.py
+++ src/Server.py
@@ -38,136 +38,62 @@ app = Flask(__name__)
 @app.route("/api/countries", methods=["POST"])
 def add_country():
 
-    params = request.get_json(silent=True)
+    return post_helper(request.get_json(silent=True), cursor, db_connection, jsonToCountry, insert_to_countries)
 
-    if not params:
-        return Response(status=400)
-
-    new_country = jsonToCountry(params)
-    if new_country is None:
-        return Response(status=400)
-
-    try:
-        id = insert_to_countries(cursor, db_connection, new_country)
-        return jsonify({'id': id})
-    except IntegrityError as e:
-        cursor.execute("ROLLBACK")
-        db_connection.commit()
-        return Response(status=409)
 
 @app.route("/api/cities", methods=["POST"])
 def add_city():
 
-    params = request.get_json(silent=True)
+    return post_helper(request.get_json(silent=True), cursor, db_connection, jsonToCity, insert_to_cities)
 
-    if not params:
-        return Response(status=400)
-
-    new_city = jsonToCity(params)
-    if new_city is None:
-        return Response(status=400)
-
-    try:
-        id = insert_to_cities(cursor, db_connection, new_city)
-        return jsonify({'id': id})
-    except IntegrityError as e:
-        cursor.execute("ROLLBACK")
-        db_connection.commit()
-        return Response(status=409)
 
 
 @app.route("/api/countries", methods=["GET"])
 def get_countries():
 
-    cursor.execute("SELECT * FROM Tari")
+    sql_command = "SELECT * FROM %s" % Constants.TARI_TABLE
 
-    countries_list = cursor.fetchall()
-
-    countries_json_list = list(map(lambda x: {"id" : x[0], "nume" : x[1], "lat" : x[2], "lon" : x[3]}, countries_list))
-
-    return jsonify(countries_json_list)
+    return get_helper(sql_command, cursor, fetched_data_to_json_countries)
 
 @app.route("/api/cities", methods=["GET"])
 def get_cities():
 
-    cursor.execute("SELECT * FROM Orase")
+    sql_command = "SELECT * FROM %s" % Constants.ORASE_TABLE
 
-    cities_list = cursor.fetchall()
-
-    cities_json_list = list(map(lambda x: {"id" : x[0], "idTara" : x[1] ,"nume" : x[2], "lat" : x[3], "lon" : x[4]}, cities_list))
-
-    return jsonify(cities_json_list)
-
-@app.route("/api/countries/<int:id>", methods=["PUT"])
-def update_country(id):
-    
-    params = request.get_json(silent=True)
-
-    if not params:
-        return Response(status=400)
-
-    updated_country = jsonToCountry(params)
-    if updated_country is None:
-        return Response(status=400)
-
-
-    cursor.execute("UPDATE TARI SET nume_tara=%s, latitudine=%s, longitudine=%s where id=%s returning id", (updated_country.name, updated_country.lat, updated_country.long, id))
-
-    if cursor.fetchone() is None:
-        return Response(status=404)
-
-    return Response(status=200)
-
-@app.route("/api/cities/<int:id>", methods=["PUT"])
-def update_city(id):
-    
-    params = request.get_json(silent=True)
-
-    if not params:
-        return Response(status=400)
-
-    updated_city = jsonToCity(params)
-    if updated_city is None:
-        return Response(status=400)
-
-    cursor.execute("UPDATE ORASE SET id_tara=%s, nume_oras=%s, latitudine=%s, longitudine=%s where id=%s returning id", (updated_city.countryID, updated_city.name, updated_city.lat, updated_city.long, id))
-
-
-    if cursor.fetchone() is None:
-        return Response(status=404)
-
-    return Response(status=200)
-
-@app.route("/api/countries/<int:id>", methods=["DELETE"])
-def delete_country(id):
-
-    cursor.execute("DELETE FROM TARI WHERE id=%s returning id", (id,))
-
-    if cursor.fetchone()[0] is None:
-        return Response(status=404)
-
-    return Response(status=200)
-
-@app.route("/api/cities/<int:id>", methods=["DELETE"])
-def delete_city(id):
-
-    cursor.execute("DELETE FROM ORASE WHERE id=%s returning id", (id,))
-
-    if cursor.fetchone()[0] is None:
-        return Response(status=404)
-
-    return Response(status=200)
+    return get_helper(sql_command, cursor, fetched_data_to_json_cities)
 
 @app.route("/api/cities/country/<int:id_Tara>", methods=["GET"])
 def get_cities_from_country(id_Tara):
     
-    cursor.execute("select * from orase where id_tara=%s", (id_Tara,))
+    sql_command = "select * from orase where id_tara=%s" %  id_Tara
 
-    cities_from_country_list = cursor.fetchall()
+    return get_helper(sql_command, cursor, fetched_data_to_json_cities)
 
-    cities_from_country_json_list = list(map(lambda x: {"id" : x[0], "idTara" : x[1] ,"nume" : x[2], "lat" : x[3], "lon" : x[4]}, cities_from_country_list))
 
-    return jsonify(cities_from_country_json_list)
+
+@app.route("/api/countries/<int:id>", methods=["PUT"])
+def update_country(id):
+
+    return put_helper(request.get_json(silent=True), id, cursor, jsonToCountry, execute_update_country, db_connection)
+
+
+@app.route("/api/cities/<int:id>", methods=["PUT"])
+def update_city(id):
+    
+    return put_helper(request.get_json(silent=True), id, cursor, jsonToCity, execute_update_city, db_connection)
+
+
+@app.route("/api/countries/<int:id>", methods=["DELETE"])
+def delete_country(id):
+
+    return delete_helper(id, cursor, Constants.TARI_TABLE, db_connection)
+
+@app.route("/api/cities/<int:id>", methods=["DELETE"])
+def delete_city(id):
+
+    return delete_helper(id, cursor, Constants.ORASE_TABLE, db_connection)
+
+
 
 
 
@@ -175,3 +101,4 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
 
     db_connection.close()
+
--- src/database/Database.py
+++ src/database/Database.py
@@ -1,3 +1,12 @@
+from flask import Flask
+from flask import request, jsonify, Response
+
+def fetched_data_to_json_countries(countries_list):
+    return list(map(lambda x: {"id" : x[0], "nume" : x[1], "lat" : x[2], "lon" : x[3]}, countries_list))
+
+def fetched_data_to_json_cities(cities_list):
+    return list(map(lambda x: {"id" : x[0], "idTara" : x[1] ,"nume" : x[2], "lat" : x[3], "lon" : x[4]}, cities_list))
+
 def insert_to_countries(cursor, db_connection, country):
 
     cursor.execute("insert into Tari (nume_tara, latitudine, longitudine) values (%s, %s, %s) returning id",
@@ -15,3 +24,62 @@ def insert_to_cities(cursor, db_connection, city):
     db_connection.commit()
 
     return cursor.fetchone()[0]
+
+def post_helper(params, cursor, db_connection, jsonToFunc, insertToFunc):
+    if not params:
+        return Response(status=400)
+
+    new_entry = jsonToFunc(params)
+    if new_entry is None:
+        return Response(status=400)
+
+    try:
+        id = insertToFunc(cursor, db_connection, new_entry)
+        return jsonify({'id': id})
+    except IntegrityError as e:
+        cursor.execute("ROLLBACK")
+        db_connection.commit()
+        return Response(status=409)
+
+def get_helper(sql_command, cursor, fetchedDataToFunc):
+    
+    cursor.execute(sql_command)
+
+    locations_list = cursor.fetchall()
+
+    locations_json_list = fetchedDataToFunc(locations_list)
+
+    return jsonify(locations_json_list)
+
+
+def execute_update_country(cursor, country, id):
+    cursor.execute("UPDATE TARI SET nume_tara=%s, latitudine=%s, longitudine=%s where id=%s returning id", (country.name, country.lat, country.long, id))
+
+def execute_update_city(cursor, city, id):
+    cursor.execute("UPDATE ORASE SET id_tara=%s, nume_oras=%s, latitudine=%s, longitudine=%s where id=%s returning id", (city.countryID, city.name, city.lat, city.long, id))
+
+def put_helper(params, id, cursor, jsonToFunc, executeUpdateFunc, db_connection):
+    
+    if not params:
+        return Response(status=400)
+
+    updated_data = jsonToFunc(params)
+    if updated_data is None:
+        return Response(status=400)
+    
+    executeUpdateFunc(cursor, updated_data, id)
+
+    if cursor.fetchone() is None:
+        return Response(status=404)
+
+    db_connection.commit()
+    return Response(status=200)
+
+def delete_helper(id, cursor, table_name, db_connection):
+    cursor.execute("DELETE FROM %s WHERE id=%s returning id" % (table_name, id,))
+
+    if cursor.fetchone()[0] is None:
+        return Response(status=404)
+    
+    db_connection.commit()
+    return Response(status=200)
